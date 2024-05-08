722ac785206ca5858638d13c22017a5b1a6e029d
gitsibi <159125779+gitsibi@users.noreply.github.com>
2024-05-08 17:45:46 +0530

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,30 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [index,setIndex]=useState(0);
+    const handlebackarrow=()=>{
+        if(index==0)
+        {
+            setIndex(images.length-1)
+        }
+        else
+        {
+            setIndex((prev)=>prev-1)
+        }
+    }
+    const handleforwardarrow=()=>{
+        index==images.length-1?setIndex(0):setIndex((prev)=>prev+1);        
+    }
+    console.log(images)
+    return(
+        <>
+            <img className="carousel-img" src={images[index].img} alt="" />
+            <ArrowBackIosIcon className="box Back-arr" onClick={handlebackarrow}/>
+            <ArrowForwardIosIcon className="box Front-arr" onClick={handleforwardarrow}/>
+            <h1 className="carousel-title">{images[index].title}</h1>
+            <p className="carousel-sub-title">{images[index].subtitle}</p>
+        </>
+    )
 }
 
 export default Carousel;
